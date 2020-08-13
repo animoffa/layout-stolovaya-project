23b5ebb43d4361786f41080a5bfaef95dca4de0a
Anastasia <animoffa@mail.ru>
2020-08-14 02:14:49 +0300

    review not finished

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
     $('#phone').mouseenter(function () {
-        $('#phone-hover').css({"display": "flex"})
+        $('#phone-hover').css({"display": "flex"});
+        $('#darken').css({"display": "initial"})
     })
     $('.search-block__search a:first-child button').click(function () {
         $('.search-block__search-hover').css({"display": "flex"})
@@ -10,13 +11,16 @@ $(document).ready(() => {
     })
     $('.categories-block div:first-child').click(function () {
         $('#hot').toggleClass("toggle-click");
+        $('#d').toggleClass("darken");
 
     })
     $('.categories-block div:nth-child(2)').click(function () {
         $('#cold').toggleClass("toggle-click");
+        $('#d').toggleClass("darken");
     })
     $('#close-form').click(function () {
-        $('#phone-hover').css({"display": "none"})
+        $('#phone-hover').css({"display": "none"});
+        $('#darken').css({"display": "none"})
     })
     $('.header_mobile img').click(function () {
         $('#header').toggleClass('active');
